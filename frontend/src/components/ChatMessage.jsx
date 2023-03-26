2ac3bdf443fa6f9d1f1c0d4f16edb6ea78f5219e
C.C.Q.A <crisfrik@gmail.com>
2023-03-26 11:56:56 -0500

    Change font

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -31,14 +31,14 @@ const ChatMessage = ({ message }) => {
         "m-2 rounded-4 bg-opacity-75 " + classes.container.layout + classes.container.color
       }
     >
-      <p className={"fs-6 fw-semibold mb-0 px-2"}>
+      <p className={"fw-light mb-0 px-2"}>
         {message.user}
         {message.userRole === "MODERATOR" && (
           <span className="float-end">Moderador</span>
         )}
       </p>
-      <p className={"fs-5 mb-1 px-2"}>{message.text}</p>
-      <p className={"fs-6 fst-italic px-2 mb-0"}>
+      <p className={"mb-1 px-3"}>{message.text}</p>
+      <p className={"fst-italic fw-light px-2 mb-0"}>
         {date.getHours() +
           ":" +
           (date.getMinutes().toString().length === 1
