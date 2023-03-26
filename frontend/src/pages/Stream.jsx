191161d89b183b27d10ce8cfb8a6ca24e63663ed
C.C.Q.A <crisfrik@gmail.com>
2023-03-25 19:34:55 -0500

    Add video

--- frontend/src/pages/Stream.jsx
+++ frontend/src/pages/Stream.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
 // Components
-import { Col } from 'react-bootstrap'
-import Chat from '../components/Chat'
-import FloatLogoutButton from '../components/FloatLogoutButton'
+import { Col } from "react-bootstrap";
+import Chat from "../components/Chat";
+import FloatLogoutButton from "../components/FloatLogoutButton";
 
 const Stream = () => {
   return (
-    <div className='vh-100 d-flex' >
-      <Col xs={8} >
-        <p>Video stream</p>
+    <div className="vh-100 d-flex">
+      <Col xs={8} className="d-flex align-items-center" >
+        <iframe
+          className="vh-85 px-3 rounded-5 w-100"
+          src="https://www.youtube.com/embed/5jaT_8hy3Vg"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        />
       </Col>
       <Chat />
       <FloatLogoutButton />
     </div>
-  )
-}
+  );
+};
 
-export default Stream
+export default Stream;
